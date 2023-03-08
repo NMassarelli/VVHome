5fdfca31586d657c093d6b4027df3557480224be
nmassarelli <noel.massarelli@gmail.com>
2023-03-08 18:38:59 -0500

    need a save all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { BookViewComponent } from './bookView/book-view/book-view.component';
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BookViewComponent, HomeComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
