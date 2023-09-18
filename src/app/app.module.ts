c257bfdee4910d743cf0b77d6359a922245f0e87
Noel Massarelli <noel.massarelli@gmail.com>
2023-09-18 17:59:38 -0400

    remove stff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,10 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TopNavComponent } from './top-nav/top-nav.component'; 
 import { FormsModule } from "@angular/forms";
-import { NewsletterModalComponent } from './newsletter-modal/newsletter-modal.component';
-import { StoryManagementComponent } from './story-management/story-management.component';
+
 
 @NgModule({
-  declarations: [AppComponent, StoryManagementComponent, LandingPageComponent, TopNavComponent],
+  declarations: [AppComponent, LandingPageComponent, TopNavComponent],
   imports: [BrowserModule,AppRoutingModule, FormsModule],
   bootstrap: [AppComponent]
 })
--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -2,8 +2,7 @@ import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 import { FontFamilyOptions } from '../Helpers/font-family-options';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { NewsletterModalComponent } from '../newsletter-modal/newsletter-modal.component';
-import { StoryManagementComponent } from '../story-management/story-management.component';
+
 @Component({
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
@@ -27,14 +26,7 @@ export class TopNavComponent {
     }
   }
  
-  launchGetEmail(){
-      this.modalService.open({
-        templateUrl: './newsletter-modal.component.html',
-        controller: NewsletterModalComponent,
-        backdrop: 'static',
-        size: 'lg'
-      });
-
-  }
-
+  
 }
+
+
